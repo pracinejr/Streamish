ce2e0224df789c10a8c59c434796ff0e896fc0d3
Peter Racine Jr <peterracinejr@gmail.com>
2021-10-05 12:10:54 -0500

    added repository files for video and tested api on postman

---
+++ Streamish/Repositories/IVideoRepository.cs
@@ -0,0 +1,14 @@
+﻿using Streamish.Models;
+using System.Collections.Generic;
+
+namespace Streamish.Repositories
+{
+    public interface IVideoRepository
+    {
+        void Add(Video video);
+        void Delete(int id);
+        List<Video> GetAll();
+        Video GetById(int id);
+        void Update(Video video);
+    }
+}
--- Streamish/Startup.cs
+++ Streamish/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Streamish.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,6 +33,9 @@ namespace Streamish
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Streamish", Version = "v1" });
             });
+
+            services.AddTransient<IVideoRepository, VideoRepository>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
