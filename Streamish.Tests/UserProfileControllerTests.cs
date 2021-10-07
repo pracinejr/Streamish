a4fc356f1a90f2a1e537c7326d93a9e373abaa87
Peter Racine Jr <peterracinejr@gmail.com>
2021-10-07 10:31:47 -0500

    added mocks file and user profile controller tests file

---
+++ Streamish.Tests/UserProfileControllerTests.cs
@@ -0,0 +1,213 @@
+﻿using Streamish.Controllers;
+using Streamish.Models;
+using Streamish.Tests.Mocks;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Streamish.Tests
+{
+    public class UserProfileControllerTests
+    {
+        [Fact]
+        public void Get_Returns_All_UserProfiles()
+        {
+            // Arrange 
+            var userProfileCount = 20;
+            var userProfiles = CreateTestUserProfiles(userProfileCount);
+
+            var repo = new InMemoryUserProfileRepository(userProfiles);
+            var controller = new UserProfileController(repo);
+
+            // Act 
+            var result = controller.Get();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var actualUserProfiles = Assert.IsType<List<UserProfile>>(okResult.Value);
+
+            Assert.Equal(userProfileCount, actualUserProfiles.Count);
+            Assert.Equal(userProfiles, actualUserProfiles);
+        }
+
+        [Fact]
+        public void Get_By_Id_Returns_NotFound_When_Given_Unknown_id()
+        {
+            // Arrange 
+            var userProfiles = new List<UserProfile>(); // no userProfiles
+
+            var repo = new InMemoryUserProfileRepository(userProfiles);
+            var controller = new UserProfileController(repo);
+
+            // Act
+            var result = controller.Get(1);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public void Get_By_Id_Returns_UserProfile_With_Given_Id()
+        {
+            // Arrange
+            var testUserProfileId = 99;
+            var userProfiles = CreateTestUserProfiles(5);
+            userProfiles[0].Id = testUserProfileId; // Make sure we know the Id of one of the userProfiles
+
+            var repo = new InMemoryUserProfileRepository(userProfiles);
+            var controller = new UserProfileController(repo);
+
+            // Act
+            var result = controller.Get(testUserProfileId);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var actualUserProfile = Assert.IsType<UserProfile>(okResult.Value);
+
+            Assert.Equal(testUserProfileId, actualUserProfile.Id);
+        }
+
+        [Fact]
+        public void Post_Method_Adds_A_New_UserProfile()
+        {
+            // Arrange 
+            var userProfileCount = 20;
+            var userProfiles = CreateTestUserProfiles(userProfileCount);
+
+            var repo = new InMemoryUserProfileRepository(userProfiles);
+            var controller = new UserProfileController(repo);
+
+            // Act
+            var newUserProfile = new UserProfile()
+            {
+                Description = "Description",
+                Title = "Title",
+                Url = "http://youtube.url?v=1234",
+                DateCreated = DateTime.Today,
+                UserProfileId = 999,
+                UserProfile = CreateTestUserProfile(999),
+            };
+
+            controller.Post(newUserProfile);
+
+            // Assert
+            Assert.Equal(userProfileCount + 1, repo.InternalData.Count);
+        }
+
+        [Fact]
+        public void Put_Method_Returns_BadRequest_When_Ids_Do_Not_Match()
+        {
+            // Arrange
+            var testUserProfileId = 99;
+            var userProfiles = CreateTestUserProfiles(5);
+            userProfiles[0].Id = testUserProfileId; // Make sure we know the Id of one of the userProfiles
+
+            var repo = new InMemoryUserProfileRepository(userProfiles);
+            var controller = new UserProfileController(repo);
+
+            var userProfileToUpdate = new UserProfile()
+            {
+                Id = testUserProfileId,
+                Description = "Updated!",
+                Title = "Updated!",
+                UserProfileId = 99,
+                DateCreated = DateTime.Today,
+                Url = "http://some.url",
+            };
+            var someOtherUserProfileId = testUserProfileId + 1; // make sure they aren't the same
+
+            // Act
+            var result = controller.Put(someOtherUserProfileId, userProfileToUpdate);
+
+            // Assert
+            Assert.IsType<BadRequestResult>(result);
+        }
+
+        [Fact]
+        public void Put_Method_Updates_A_UserProfile()
+        {
+            // Arrange
+            var testUserProfileId = 99;
+            var userProfiles = CreateTestUserProfiles(5);
+            userProfiles[0].Id = testUserProfileId; // Make sure we know the Id of one of the userProfiles
+
+            var repo = new InMemoryUserProfileRepository(userProfiles);
+            var controller = new UserProfileController(repo);
+
+            var userProfileToUpdate = new UserProfile()
+            {
+                Id = testUserProfileId,
+                Description = "Updated!",
+                Title = "Updated!",
+                UserProfileId = 99,
+                DateCreated = DateTime.Today,
+                Url = "http://some.url",
+            };
+
+            // Act
+            controller.Put(testUserProfileId, userProfileToUpdate);
+
+            // Assert
+            var userProfileFromDb = repo.InternalData.FirstOrDefault(p => p.Id == testUserProfileId);
+            Assert.NotNull(userProfileFromDb);
+
+            Assert.Equal(userProfileToUpdate.Description, userProfileFromDb.Description);
+            Assert.Equal(userProfileToUpdate.Title, userProfileFromDb.Title);
+            Assert.Equal(userProfileToUpdate.UserProfileId, userProfileFromDb.UserProfileId);
+            Assert.Equal(userProfileToUpdate.DateCreated, userProfileFromDb.DateCreated);
+            Assert.Equal(userProfileToUpdate.Url, userProfileFromDb.Url);
+        }
+
+        [Fact]
+        public void Delete_Method_Removes_A_UserProfile()
+        {
+            // Arrange
+            var testUserProfileId = 99;
+            var userProfiles = CreateTestUserProfiles(5);
+            userProfiles[0].Id = testUserProfileId; // Make sure we know the Id of one of the userProfiles
+
+            var repo = new InMemoryUserProfileRepository(userProfiles);
+            var controller = new UserProfileController(repo);
+
+            // Act
+            controller.Delete(testUserProfileId);
+
+            // Assert
+            var userProfileFromDb = repo.InternalData.FirstOrDefault(p => p.Id == testUserProfileId);
+            Assert.Null(userProfileFromDb);
+        }
+
+        private List<UserProfile> CreateTestUserProfiles(int count)
+        {
+            var userProfiles = new List<UserProfile>();
+            for (var i = 1; i <= count; i++)
+            {
+                userProfiles.Add(new UserProfile()
+                {
+                    Id = i,
+                    Description = $"Description {i}",
+                    Title = $"Title {i}",
+                    Url = $"http://youtube.url/{i}?v=1234",
+                    DateCreated = DateTime.Today.AddDays(-i),
+                    UserProfileId = i,
+                    UserProfile = CreateTestUserProfile(i),
+                });
+            }
+            return userProfiles;
+        }
+
+        private UserProfile CreateTestUserProfile(int id)
+        {
+            return new UserProfile()
+            {
+                Id = id,
+                Name = $"User {id}",
+                Email = $"user{id}@example.com",
+                DateCreated = DateTime.Today.AddDays(-id),
+                ImageUrl = $"http://user.url/{id}",
+            };
+        }
+    }
+}
